f68d9296ac5053c71707db5bc35b82051bc6339b
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-03-21 08:17:26 +0200

    update

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double speakingMark, readingMark, writingMark, listeningMark;
+
+        Console.Clear();
+        Console.Write("Speaking ballarini kiriting: ");
+        speakingMark = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Reading ballarini kiriting: ");
+        readingMark = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Writing ballarini kiriting: ");
+        writingMark = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Listening ballarini kiriting: ");
+        listeningMark = Convert.ToDouble(Console.ReadLine());
+
+        double totalMarks = speakingMark + readingMark + writingMark + listeningMark;
+
+        double averageMark = totalMarks / 4;
+
+        Console.WriteLine($"Ortacha bal: {averageMark}");
+
+        Console.Write("Davom etasizmi? (yes/no): ");
+        string continueInput = Console.ReadLine();
+
+        if (continueInput.ToLower() == "yes")
+        {
+            Main();
+        }
+        else if (continueInput.ToLower() == "no")
+        {
+            Console.WriteLine("Dastur to'xtatildi.");
+        }
+        else
+        {
+            Console.WriteLine("error!!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Ielts-score
+
+Salom bu mening yangi proyektim va bu proyekt sizga **Ielts** balingizni hisoblab berishga yordam beradi...
+
+## **Code**ni ishlatish
+
+![run code](/image/gif1.gif)
+### Tarifi 
+Bu yerda siz birinchi bo'lib **speking** balingizni kiriting keyin **reading** baligizni keyin esa **writing** balingizni va ohirida **listening** balingizni kiriting lekin shoshilmang!! Siz **listening** balingizni kiritganingizdan so'ng enter bosishga shoshilmang birinchi navbatda hamma blingiz to'griligiga ishonch hosil qiling va **enter** ni bosing.
+
+## Qo'shimcha
+
+Agarda siz tepadagi **gif**ga etibor bersangiz pastda **Davom etasizmi? (yes/no):**  degan jumlani ko'rgan bo'lsangiz kerak. 
+![run code](/image/gif2.gif)
+Bu yerda **yes**ni kiritsangiz siz dasturni qayta ishga tushirasiz.
+
+![run code](/image/gif3.gif)
+Buyerda siz **no**ni kiritsangiz daturni to'htatasiz.
+
+Lekin boshqa turdagi qo'zlarni kiritsangiz:
+![run code](/image/gif4.gif)
+Mana shunday bo'ladi.
+
+# Made by @KamronCoder2011
